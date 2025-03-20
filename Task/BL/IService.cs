f5ea20b2063b28461fa4c85c8a82ed0deda3b337
AbdelrahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2025-03-20 09:29:30 +0200

    Exploring a service abstraction

---
+++ Task/BL/IService.cs
@@ -0,0 +1,9 @@
+﻿namespace Task.BL
+{
+    public interface IService<ViewModel>
+    {
+        public void Create(ViewModel Data);
+        public void Update(ViewModel Data, int Id);
+        public void Delete(int Id);
+    }
+}
