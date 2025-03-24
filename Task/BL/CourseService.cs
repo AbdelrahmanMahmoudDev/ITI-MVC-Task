3e76349e3fba54e28b4c88b676d145d630eec923
AbdelrahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2025-03-25 00:08:36 +0200

    Added course and department cruds

---
+++ Task/BL/CourseService.cs
@@ -0,0 +1,112 @@
+﻿using System.Diagnostics;
+using Task.Models;
+using Task.Repositories.Base;
+using Task.ViewModels;
+
+namespace Task.BL
+{
+    public class CourseService : ICourseService<CourseVM>
+    {
+        private readonly IUnitOfWork _UnitOfWork;
+        public CourseService(IUnitOfWork UnitOfWork)
+        {
+            _UnitOfWork = UnitOfWork ?? throw new ArgumentNullException(nameof(UnitOfWork));
+        }
+        public async System.Threading.Tasks.Task CreateAsync(CourseVM Data)
+        {
+            Course NewCourse = new Course
+            {
+                name = Data.Name,
+                topic = Data.Topic,
+                MinimumDegree = Data.MinimumDegree
+            };
+
+            try
+            {
+                _UnitOfWork.Courses.Create(NewCourse);
+                await _UnitOfWork.Courses.UploadToDatabaseAsync();
+            }
+            catch (Exception Ex)
+            {
+                Debug.WriteLine(Ex.Message);
+            }
+        }
+
+        public async System.Threading.Tasks.Task DeleteAsync(int Id)
+        {
+            Course TargetCourse = _UnitOfWork.Courses.GetById(Id);
+
+            if (TargetCourse == null)
+            {
+                Debug.WriteLine($"Entity with Id {Id} does not exist.");
+                throw new InvalidOperationException();
+            }
+
+            try
+            {
+                _UnitOfWork.Courses.Delete(TargetCourse);
+                await _UnitOfWork.Courses.UploadToDatabaseAsync();
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine($"Deleting entity {Id} failed.");
+                throw new InvalidOperationException();
+            }
+        }
+
+        public IEnumerable<Course> PrepareDashboard()
+        {
+            IEnumerable<Course> Result = _UnitOfWork.Courses.GetAll();
+
+            return Result;
+        }
+
+        public CourseVM PrepareEdit(int Id)
+        {
+            Course TargetCourse = _UnitOfWork.Courses.GetById(Id);
+
+            if (TargetCourse == null)
+            {
+                Debug.WriteLine($"Entity with given Id: {Id} does not exist.");
+
+                throw new NullReferenceException($"Entity with given Id: {Id} does not exist.");
+            }
+
+            CourseVM TargetViewModel = new CourseVM()
+            {
+                Id = TargetCourse.CourseId,
+                Name = TargetCourse.name,
+                Topic = TargetCourse.topic,
+                MinimumDegree = TargetCourse.MinimumDegree,
+            };
+
+            return TargetViewModel;
+        }
+
+        public async System.Threading.Tasks.Task UpdateAsync(CourseVM Data, int Id)
+        {
+            Course TargetCourse = _UnitOfWork.Courses.GetById(Id);
+
+            if (TargetCourse == null)
+            {
+                Debug.WriteLine($"An entity with Id {Id} does not exist");
+                throw new NullReferenceException();
+            }
+
+            TargetCourse.name = Data.Name;
+            TargetCourse.topic = Data.Topic;
+            TargetCourse.MinimumDegree = Data.MinimumDegree;
+
+            try
+            {
+                _UnitOfWork.Courses.Update(TargetCourse);
+                await _UnitOfWork.Courses.UploadToDatabaseAsync();
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine($"Updating Entity {Id} failed");
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
---
+++ Task/Controllers/CourseController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task.BL;
+using Task.Models;
+using Task.ViewModels;
+
+namespace Task.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService<CourseVM> _CourseService;
+
+        public CourseController(ICourseService<CourseVM> CourseService)
+        {
+            _CourseService = CourseService ?? throw new ArgumentNullException(nameof(CourseService));
+        }
+
+        public IActionResult Index()
+        {
+            List<Course> Courses = _CourseService.PrepareDashboard().ToList();
+            return View("Index", Courses);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SaveAdd(CourseVM FormData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Add", FormData);
+            }
+
+            try
+            {
+                await _CourseService.CreateAsync(FormData);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occured while processing your request.");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int Id)
+        {
+            try
+            {
+                CourseVM CourseModel = _CourseService.PrepareEdit(Id);
+                return View(CourseModel);
+            }
+            catch(NullReferenceException)
+            {
+                return StatusCode(500, "An error occured while processing your request.");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SaveEdit(int Id, CourseVM FormData)
+        {
+            if(!ModelState.IsValid)
+            {
+                try
+                {
+                    CourseVM CourseModel = _CourseService.PrepareEdit(Id);
+                    return View(CourseModel);
+                }
+                catch(NullReferenceException)
+                {
+                    return StatusCode(500, "An error occured while processing your request.");
+                }
+            }
+
+            try
+            {
+                await _CourseService.UpdateAsync(FormData, Id);
+                return RedirectToAction("Index");
+            }
+            catch(InvalidOperationException)
+            {
+                return StatusCode(500, "An error occured while processing your request.");
+            }
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            try
+            {
+                await _CourseService.DeleteAsync(Id);
+            }
+            catch (InvalidOperationException)
+            {
+                return StatusCode(500, "An error occured processing your request.");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
